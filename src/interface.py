5f98957047b7a1cbfcd87b8185e11627c8b85da4
rmshimomura <shimomurarodrigo1@gmail.com>
2022-05-10 23:12:58 -0300

    Added video list, resizable, added info to button

--- src/interface.py
+++ src/interface.py
@@ -14,14 +14,15 @@ class App(Tk):
     def __init__(self):
 
         super().__init__()
-        self.title('Gerar relatórios das clínicas')
-        self.geometry("900x460")
+        self.title('Baixar músicas do YouTube')
+        self.geometry("900x500")
         self.configure(background='#FFFFFF')
 
         self.output_path = None
         self.urls = {}
 
         self.audio_formats = {
+            'Selecione formato desejado...': 0,
             '.flac': 'flac',
             '.mp3': 'mp3',
             '.wav': 'wav',
@@ -37,12 +38,6 @@ class App(Tk):
         if os.path.isfile(f'./default_output_path.txt'):
             self.output_path = open("default_output_path.txt", "r").read()
 
-        if self.output_path is not None:
-            self.current_output_path = Label(
-                self, text=f"Pastas de saída: {self.output_path}", bg="white")
-            self.current_output_path.grid(
-                row=3, column=0, pady=10, columnspan=3, sticky="W")
-
     def store_output_path(self):
 
         self.output_path = filedialog.askdirectory(
@@ -54,31 +49,40 @@ class App(Tk):
             if os.path.isfile(f'./default_output_path.txt'):
                 self.output_path = open("default_output_path.txt", "r").read()
 
-        if self.output_path is not None and self.output_path != "":
-            self.current_output_path = Label(
-                self, text=f"Pasta de saída: {self.output_path}", bg="white")
-            self.current_output_path.grid(
-                row=3, column=0, pady=10, columnspan=3, sticky="W")
-
     def addURL(self, event):
 
-        if self.url.get() != "": # Check if the url is not empty
+        if self.url.get() != "":  # Check if the url is not empty
 
-            if self.url.get() not in self.urls: # Check if the url is not already in the list
+            if self.url.get() not in self.urls:  # Check if the url is not already in the list
 
-                if self.url.get().startswith("https://www.youtube.com/watch?v="): # Check if the url is a valid youtube url
+                if self.url.get().startswith("https://www.youtube.com/watch?v="):  # Check if the url is a valid youtube url
 
-                    if requests.get(self.url.get()).status_code == 200: # Check if the url is valid
+                    # Check if the url is valid
+                    if requests.get(self.url.get()).status_code == 200:
 
                         video_name = return_video_name(self.url.get())
 
-                        self.urls[self.url.get()] = video_name # Add the url to the list
+                        # Add the url to the list
+                        self.urls[self.url.get()] = video_name
 
-                        messagebox.showinfo('Sucesso', f'Link para o vídeo "{video_name}" adicionado!') # Show a message to the user
+                        # Show a message to the user
+                        messagebox.showinfo(
+                            'Sucesso', f'Link para o vídeo "{video_name}" adicionado!')
+
+                        if len(self.urls) > 0:
+                            self.links_title = Label(self, text="Links adicionados:", bg="white").grid(row=4, column=0, padx=10, pady=10, sticky="W")
+                            self.list_of_urls = Listbox(self.links_title, height=10, width=60, highlightbackground = "black", 
+                         highlightthickness = 2)
+                            
+                            for url in self.urls.values():
+                                self.list_of_urls.insert(END, url)
+                            self.list_of_urls.grid(
+                                row=5, column=0, padx=10, pady=10, sticky="W")
+                            self.list_of_urls.configure(background="#ffffff")
 
     def render_buttons(self):
 
-        self.out_dir = Button(self, text="Selecione a pasta de saída...",
+        self.out_dir = Button(self, text=f"Selecione a pasta de saída...{chr(10) + chr(10) + 'Atual --> ' + self.output_path if self.output_path is not None and self.output_path != '' else ''}",
                               command=self.store_output_path, height=6, width=60, borderwidth=0)
         self.out_dir.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
         self.out_dir.configure(background="#ff6e6c")
@@ -95,9 +99,9 @@ class App(Tk):
         self.open_output_dir.grid(
             row=2, column=1, padx=10, pady=10, sticky="nsew")
         self.open_output_dir.configure(background="#ff6e6c")
-        
+
         self.execute_action = Button(self, text="Baixar músicas", command=lambda: download(
-            self.urls.keys(), self.audio_formats[self.format_selected.get()], self.output_path), height=6, width=60, borderwidth=0)
+            self.urls, self.audio_formats[self.format_selected.get()], self.output_path), height=6, width=60, borderwidth=0)
         self.execute_action.grid(
             row=2, column=0, padx=10, pady=10, sticky="nsew")
         self.execute_action.configure(background="#ff6e6c")
@@ -115,7 +119,8 @@ class App(Tk):
 
         self.render_buttons()
 
-        self.resizable(width="false", height="false")
+        self.resizable(width="false", height="true")
+
 
 if __name__ == "__main__":
     app = App()
